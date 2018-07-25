301500b9e0ca1082ccd41c46fc3b786eec62710c
Marc Wood <marcdw87@gmail.com>
2018-07-25 12:40:28 -0500

    Start using a create_app function.

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+def create_app():
+    return app
+
 from server import routes
--- server/__main__.py
+++ server/__main__.py
@@ -1,4 +1,5 @@
-from server import app
+from server import create_app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
