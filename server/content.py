132c33d5045a5a0b8291746829c1bfa3ad167668
Marc Wood <marcdw87@gmail.com>
2018-07-27 22:37:44 -0500

    Add project cards and routes. Add a footer.

---
+++ server/content.py
@@ -0,0 +1,45 @@
+
+projects = [
+    {
+        "title": "Nowallet",
+        "imgfile": "nowallet.png",
+        "url": "nowallet",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    },
+    {
+        "title": "Component.py",
+        "imgfile": "component.jpg",
+        "url": "component",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    },
+    {
+        "title": "Python/Fullstack/Transcrypt",
+        "imgfile": "fullstack.png",
+        "url": "fullstack",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    },
+    {
+        "title": "CHIP8",
+        "imgfile": "chip8.png",
+        "url": "chip8",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    },
+    {
+        "title": "Bitmessage over I2P",
+        "imgfile": "bitmessage.png",
+        "url": "bitmessage",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    },
+    {
+        "title": "METAmarket",
+        "imgfile": "mm_logo.jpg",
+        "url": "metamarket",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                        "Similique ipsam cupiditate voluptates beatae vitae iusto ut"
+    }
+]
--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,32 @@
 from flask import render_template, flash
 from server import app
+from server.content import projects
 
 @app.route("/")
 def index():
-    flash("This is a flash message.")  # TODO: Remove example flash
-    return render_template("index.html", title="Portfolio")
+    flash("Thanks for visiting. Please make yourself comfortable :P")
+    return render_template("index.html", title="Portfolio", projects=projects)
+
+@app.route("/nowallet")
+def nowallet():
+    return render_template("project.html", data=projects[0])
+
+@app.route("/component")
+def component():
+    return render_template("project.html", data=projects[1])
+
+@app.route("/fullstack")
+def fullstack():
+    return render_template("project.html", data=projects[2])
+
+@app.route("/chip8")
+def chip8():
+    return render_template("project.html", data=projects[3])
+
+@app.route("/bitmessage")
+def bitmessage():
+    return render_template("project.html", data=projects[4])
+
+@app.route("/metamarket")
+def metamarket():
+    return render_template("project.html", data=projects[5])
