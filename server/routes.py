26b2f9fed945a6fd9395f890c69b0795810a23de
Marc Wood <marcdw87@gmail.com>
2018-07-28 23:00:45 -0500

    Change blueprint name to portfolio. Formatting.

--- server/routes.py
+++ server/routes.py
@@ -3,18 +3,25 @@ import random
 from flask import Blueprint, render_template, flash
 from server.content import projects, rights
 
-portfolio = Blueprint("simple_page", __name__, template_folder="templates")
+portfolio = Blueprint("portfolio", __name__, template_folder="templates")
+
 
 @portfolio.route("/")
 def index():
     flash("Thanks for visiting. Please make yourself comfortable :P")
-    return render_template(
-        "index.html", title="Portfolio", projects=projects, right=random.choice(rights))
+    return render_template("index.html",
+                           title="Portfolio",
+                           projects=projects,
+                           right=random.choice(rights))
+
 
 @portfolio.route("/projects/<int:id>")
 def projects_view(id):
-    return render_template(
-        "project.html", title=projects[id]["title"], project=projects[id], right=random.choice(rights))
+    return render_template("project.html",
+                           title=projects[id]["title"],
+                           project=projects[id],
+                           right=random.choice(rights))
+
 
 @portfolio.route("/contact")
 def contact_view():
