807fb93059c019c0ce8b263bc03b1c86add7885b
Marc Wood <marcdw87@gmail.com>
2018-08-02 20:56:47 -0500

    Separate new and edit routes. Improve login dialog.

--- server/routes.py
+++ server/routes.py
@@ -60,9 +60,8 @@ def contact_view():
 
 
 #pylint: disable=E1101
-@portfolio.route("/edit", methods=["GET", "POST"])
-@portfolio.route("/edit/<int:id>", methods=["GET", "POST"])
-def edit_project(id=None):
+@portfolio.route("/new", methods=["GET", "POST"])
+def new_project():
     from server import db
     from server.models import Project
     from server.forms import ProjectForm
@@ -70,34 +69,49 @@ def edit_project(id=None):
     if not logged_in:
         abort(403)
 
-    project = None
-    if id:
-        project = Project.query.filter_by(id=id).first()
-        project_form = ProjectForm(obj=project)
-    else:
-        project_form = ProjectForm()
-
+    project_form = ProjectForm()
     if request.method == "POST" and project_form.validate():
-        if project:
-            project_form.populate_obj(project)
-            db.session.commit()
-        else:
-            new_project = Project(title=request.form["title"],
-                                  imgfile=request.form["imgfile"],
-                                  website=request.form["website"],
-                                  github_url=request.form["github_url"],
-                                  abandoned=request.form.get(
-                                      "abandoned") is not None,
-                                  description=request.form["description"],
-                                  long_desc=request.form["long_desc"])
-            db.session.add(new_project)
-            db.session.commit()
-        flash("Edit was successful.")
+        project = Project(title=request.form["title"],
+                                imgfile=request.form["imgfile"],
+                                website=request.form["website"],
+                                github_url=request.form["github_url"],
+                                abandoned=request.form.get(
+                                    "abandoned") is not None,
+                                description=request.form["description"],
+                                long_desc=request.form["long_desc"])
+        db.session.add(project)
+        db.session.commit()
+        flash("Project was created.")
         return redirect(url_for("portfolio._portfolio"))
 
     return render_template("edit_project.html",
                            form=project_form,
                            title="Create Projects",
+                           right=random.choice(rights),
+                           logged_in=logged_in)
+
+
+@portfolio.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_project(id):
+    from server import db
+    from server.models import Project
+    from server.forms import ProjectForm
+
+    if not logged_in:
+        abort(403)
+
+    project = Project.query.filter_by(id=id).first()
+    project_form = ProjectForm(obj=project)
+
+    if request.method == "POST" and project_form.validate():
+        project_form.populate_obj(project)
+        db.session.commit()
+        flash("Edit was successful.")
+        return redirect(url_for("portfolio._portfolio"))
+
+    return render_template("edit_project.html",
+                           form=project_form,
+                           title="Edit Projects",
                            right=random.choice(rights),
                            logged_in=logged_in)
 
@@ -112,7 +126,7 @@ def delete_project(id):
     db.session.delete(project)
     db.session.commit()
     flash("Delete was successful.")
-    return redirect(url_for("portfolio.home"))
+    return redirect(url_for("portfolio._portfolio"))
 
 
 @portfolio.route("/logout")
