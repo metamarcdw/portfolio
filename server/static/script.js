5a28f294604f5faa007b8f27a307066eab327d65
Marc Wood <marcdw87@gmail.com>
2018-08-18 02:27:40 -0500

    Add jQuery for SimpleMDE and login button fading.

--- server/static/script.js
+++ server/static/script.js
@@ -7,4 +7,16 @@ $(function() {
     $(".delete-btn").on("click", function() {
         return window.confirm("Are you sure?");
     });
+
+    var element = $(".simplemde")[0];
+    if (element) {
+        var simplemde = new SimpleMDE({element});
+    }
+
+    $(document).keypress("q", function(e) {
+        if (e.ctrlKey) {
+            $("#btn-login").fadeToggle("slow");
+        }
+    });
+
 });
