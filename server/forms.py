7dc2edb918d8491fe3061a04275635a319617df6
Marc Wood <marcdw87@gmail.com>
2018-08-18 02:26:51 -0500

    Add BlogpostForm class.

--- server/forms.py
+++ server/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms_alchemy import model_form_factory
 
 from server import db
-from server.models import Project
+from server.models import Project, Blogpost
 
 BaseModelForm = model_form_factory(FlaskForm)
 
@@ -19,3 +19,11 @@ class ProjectForm(ModelForm):
     field_names = ("title", "website",
                    "github_url", "abandoned",
                    "description", "long_desc")
+
+
+class BlogpostForm(ModelForm):
+    class Meta:
+        model = Blogpost
+        exclude = ["imgfile"]
+
+    field_names = ("title", "markdown")
