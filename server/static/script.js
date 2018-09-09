67acbb73d293b30701a128ee676ce09d715375d2
Marc Wood <marcdw87@gmail.com>
2018-09-09 00:03:42 -0500

    Put code into IIFE and use strict mode.

--- server/static/script.js
+++ server/static/script.js
@@ -1,34 +1,38 @@
+'use strict';
+(function() {
 
-var backlink = document.getElementById('backlink');
-if (backlink) {
-    backlink.addEventListener('click', function() {
-        window.location.href = document.referrer;
-    });
-}
-
-var deleteButtons = document.getElementsByClassName('delete-btn');
-if (deleteButtons) {
-    Array.prototype.forEach.call(deleteButtons, function(button) {
-        button.addEventListener('click', function(e) {
-            if (!window.confirm('Are you sure?'))
-                e.preventDefault();
+    var backlink = document.getElementById('backlink');
+    if (backlink) {
+        backlink.addEventListener('click', function() {
+            window.location.href = document.referrer;
         });
-    });
-}
+    }
 
-var textArea = document.getElementsByClassName('simplemde')[0];
-if (textArea)
-    var simplemde = new SimpleMDE({textArea});
+    var deleteButtons = document.getElementsByClassName('delete-btn');
+    if (deleteButtons) {
+        Array.prototype.forEach.call(deleteButtons, function(button) {
+            button.addEventListener('click', function(e) {
+                if (!window.confirm('Are you sure?'))
+                    e.preventDefault();
+            });
+        });
+    }
 
-document.addEventListener('keypress', function(e) {
-    if (e.ctrlKey && e.key === 'q') {
-        var loginBtn = document.getElementById('btn-login');
-        if (loginBtn) {
-            if (loginBtn.classList.contains('hidden')) {
-                loginBtn.classList.remove('hidden');
-            } else {
-                loginBtn.classList.add('hidden');
+    var textArea = document.getElementsByClassName('simplemde')[0];
+    if (textArea)
+        var simplemde = new SimpleMDE({textArea});
+
+    document.addEventListener('keypress', function(e) {
+        if (e.ctrlKey && e.key === 'q') {
+            var loginBtn = document.getElementById('btn-login');
+            if (loginBtn) {
+                if (loginBtn.classList.contains('hidden')) {
+                    loginBtn.classList.remove('hidden');
+                } else {
+                    loginBtn.classList.add('hidden');
+                }
             }
         }
-    }
-});
+    });
+
+})();
